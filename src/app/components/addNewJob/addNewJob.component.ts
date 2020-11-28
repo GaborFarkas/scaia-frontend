daebb607d5c058f2e44260a5cf88f9a7629ed473
Bence Nagy <nabsabt@gmail.com>
2020-11-28 13:07:06 +0100

    ol map added/addNewJob button

--- src/app/components/addNewJob/addNewJob.component.ts
+++ src/app/components/addNewJob/addNewJob.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HomePage } from 'src/app/pages/home';
 
 @Component({
   selector: 'app-addNewJob',
   templateUrl: './addNewJob.component.html',
   styleUrls: ['./addNewJob.component.css']
 })
+
 export class AddNewJobComponent {
+
   content_title1 = "Add new job";
   ngOnInit(): void {
   }
+
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,35 +1,55 @@
+import { trigger } from '@angular/animations';
 import { Component, EventEmitter, NgModule, Output, ViewChild, ViewEncapsulation, AfterViewInit, ElementRef } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import ZoomToExtent from 'ol/control/ZoomToExtent';
+import { defaults as defaultControls } from 'ol/control';
 
 @Component({
   templateUrl: 'home.page.html' ,
   styleUrls: [ './home.page.css' ],
   encapsulation: ViewEncapsulation.None
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  @ViewChild('main_field', { static: true }) field: ElementRef;
-  public toggleVisibility: boolean = false; //if "false", field is visible
+  map: Map;
 
-  hideToggle() {
-
-    if(this.toggleVisibility == true){
-      this.field.nativeElement.style="display:none";
-      this.toggleVisibility = ! this.toggleVisibility;
-
-    }else{
-      this.field.nativeElement.style="display:block";
-      this.toggleVisibility = ! this.toggleVisibility;
-    }
+  ngAfterViewInit() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [2213079.7791264898, 5939220.284081122],
+        zoom: 8
+      }),
+      controls: defaultControls().extend([
+        new ZoomToExtent({
+          extent: [
+            2053079.7791264898, 5719220.284081122,
+            2383079.7791264898, 6209220.284081122
+          ]
+        })
+      ])
+    });
   }
 
-
-
   public mode: string = "";
-  @Output() modeChanged = new EventEmitter<string>();
-
 
   public changeMode(newMode: string ) {
-    this.mode = newMode;
+    if(this.mode === newMode) {
+      this.mode = "";
+    } else {
+      this.mode = newMode;
+    }
 }
 
 }
