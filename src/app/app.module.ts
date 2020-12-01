d10f3b4e6056610b790b4ab49c2a89a3f34c9d5d
GaborFarkas <raandal73@gmail.com>
2020-12-01 22:33:18 +0100

    Add map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HelpComponent } from './components/help';
 import { TutorialComponent } from './components/tutorial';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MapComponent } from './components/map';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     DataDownloadComponent,
     ContactComponent,
     HelpComponent,
-    TutorialComponent
+    TutorialComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,41 @@
+import { AfterViewInit, Component } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import ZoomToExtent from 'ol/control/ZoomToExtent';
+import { defaults as defaultControls } from 'ol/control';
+
+@Component({
+    selector: 'app-map',
+    templateUrl: 'map.component.html',
+    styleUrls: [ 'map.component.css' ]
+})
+export class MapComponent implements AfterViewInit {
+    private map: Map;
+
+    ngAfterViewInit(): void {
+        this.map = new Map({
+            target: 'olMap',
+            layers: [
+              new TileLayer({
+                source: new XYZ({
+                  url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                })
+              })
+            ],
+            view: new View({
+              center: [2213079.7791264898, 5939220.284081122],
+              zoom: 8
+            }),
+            controls: defaultControls().extend([
+              new ZoomToExtent({
+                extent: [
+                  2053079.7791264898, 5719220.284081122,
+                  2383079.7791264898, 6209220.284081122
+                ]
+              })
+            ])
+        });
+    }
+}
