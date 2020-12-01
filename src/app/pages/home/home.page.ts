419938d647bd216f26c1752bd11994c28576b489
GaborFarkas <raandal73@gmail.com>
2020-12-01 22:33:44 +0100

    Include map component in home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,4 @@
-import { trigger } from '@angular/animations';
-import { Component, EventEmitter, NgModule, Output, ViewChild, ViewEncapsulation, AfterViewInit, ElementRef, OnInit } from '@angular/core';
-
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
-import ZoomToExtent from 'ol/control/ZoomToExtent';
-import { defaults as defaultControls } from 'ol/control';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import AuthService from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { UserData } from 'src/app/models/userdata.model';
@@ -16,9 +8,7 @@ import { UserData } from 'src/app/models/userdata.model';
   styleUrls: [ './home.page.css' ],
   encapsulation: ViewEncapsulation.None
 })
-export class HomePage implements AfterViewInit, OnInit {
-
-  private map: Map;
+export class HomePage implements OnInit {
   private userData: UserData;
 
   public get userName(): string {
@@ -37,31 +27,6 @@ export class HomePage implements AfterViewInit, OnInit {
       } else {
         this.userData = data.userData;
       }
-    });
-  }
-
-  ngAfterViewInit() {
-    this.map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          })
-        })
-      ],
-      view: new View({
-        center: [2213079.7791264898, 5939220.284081122],
-        zoom: 8
-      }),
-      controls: defaultControls().extend([
-        new ZoomToExtent({
-          extent: [
-            2053079.7791264898, 5719220.284081122,
-            2383079.7791264898, 6209220.284081122
-          ]
-        })
-      ])
     });
   }
 
