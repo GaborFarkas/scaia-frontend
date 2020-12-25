8df63e5a29a42f9d66f2d1e4687f2146eb879670
GaborFarkas <raandal73@gmail.com>
2020-12-25 19:28:58 +0100

    Use config in new job comp

--- src/app/components/addNewJob/addNewJob.component.ts
+++ src/app/components/addNewJob/addNewJob.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
 import { HomePage } from 'src/app/pages/home';
+import ConfigService from 'src/app/services/config.service';
 
 @Component({
   selector: 'app-addNewJob',
@@ -7,10 +11,30 @@ import { HomePage } from 'src/app/pages/home';
   styleUrls: ['./addNewJob.component.css']
 })
 
-export class AddNewJobComponent {
-
+export class AddNewJobComponent implements OnInit {
   content_title1 = "Add new job";
+  public config: Product[];
+
+  constructor(
+    private configService: ConfigService,
+    private router: Router) {}
+
   ngOnInit(): void {
+    this.getConfig();
+  }
+
+  /**
+   * Saves the product configuration to generate the buttons from.
+   */
+  private async getConfig() {
+    try {
+      this.config = await this.configService.getProductsAsync();
+    } catch (ex) {
+      // If we get an Unauthorized response, the user is not allowed to use the app. Go back to the login page.
+      if (ex.status === 401) {
+        this.router.navigate(["login"]);
+      }
+    }
   }
 
 }
